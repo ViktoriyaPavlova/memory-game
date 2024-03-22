744ebf18dff113144beed0275aff60bbaddeceb3
ViktoriyaPavlova <ophtalma@yandex.ru>
2024-03-22 15:33:49 +0300

    [MEMORY-GAME]-001.  Получение игровых селекторов. Создание базового стейта. Функция генерации игрового поля (WIP).

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,57 @@
-// your code ["🥔", "🍒", "🥑", "🌽", "🥕", "🍇", "🍉", "🍌", "🥭", "🍍"]
+const EMOJIS = ["🥔", "🍒", "🥑", "🌽", "🥕", "🍇", "🍉", "🍌", "🥭", "🍍"];
+
+/**
+ * Состояние игры
+ * @property {boolean} isGameStarted- Игра началась или нет.
+ * @property {number} totalTime - Общее время в игре.
+ * @property {number} flippedCards - Количество перевернутых карточек.
+ * @property {number} totalFlips - Общее количество перевернутых карточек.
+ */
+const STATE = {
+  isGameStarted: false,
+  totalTime: 0,
+  flippedCards: 0,
+  totalFlips: 0,
+};
+
+/**
+ * Контролы игры
+ * @property {HTMLDivElement} boardContainer - Контейнер игрового поля.
+ * @property {HTMLDivElement} board - Основное содержимое поля.
+ * @property {HTMLDivElement} moves - Контрол для учета шагов.
+ * @property {HTMLDivElement} timer - Контрол для учета времени.
+ * @property {HTMLButtonElement} start - Кнопка для старта игры.
+ */
+const SELECTORS = {
+  boardContainer: document.querySelector(".board-container"),
+  board: document.querySelector(".board"),
+  moves: document.querySelector(".moves"),
+  timer: document.querySelector(".timer"),
+  start: document.querySelector("button"),
+};
+
+/**
+ * Генерация игрового поля
+ */
+const generateGame = () => {
+  //Получение data атрибута
+  const dimensions = SELECTORS.board.dataset.dimension;
+
+  if (dimensions % 2 !== 0) {
+    throw new Error("Размер игрового поля должен быть четным!");
+  }
+  // Итерация по карточкам
+  const cardsHTML = EMOJIS.map((emoji) => {
+    return `
+        <div class ="card">
+           <div class ="card-front"></div>
+           <div class="card-back">${emoji}</div>
+        </div>`;
+  }).join("");
+
+  // Вставка карточек в игровое поле
+  SELECTORS.board.insertAdjacentHTML("beforeend", cardsHTML);
+  console.log(cardsHTML);
+};
+
+generateGame();
